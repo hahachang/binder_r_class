995bd791d4e5c67b655c04359533c54ff0e54d27
hahachang <hahachang@gmail.com>
2021-04-20 13:01:45 +0800

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,9 @@
 #install.packages("Rcpp", repos="https://rcppcore.github.io/drat")
+install.packages("showtext")
 install.packages("magrittr")
 install.packages("data.table")
 install.packages("ggplot2")
-install.packages("showtext")
+
 #install.packages("readxl")
 
 #install.packages("tidyverse")
